836d91c933b445c412065218dffe76999e72298c
MariaDzha <m_zudina@inbox.ru>
2021-12-10 23:17:41 +0300

    files added

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,14 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calculateSQR(int lowBorder, int topBorder) {
+        int i;
+        int counter = 0;
+        for (i = 10; i <= 99; i++) {
+            if (i * i >= lowBorder && i * i <= topBorder) {
+                counter++;
+            }
+           }
+        return counter;
+    }
+}
---
+++ src/test/java/SQRServiceTests/Test.java
@@ -0,0 +1,26 @@
+package SQRServiceTests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.sqr.SQRService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SQRServiceTests {
+    @ParameterizedTest
+    @CsvSource(
+            value={
+                    "0,10,0",
+                    "100,100,1",
+                    "100,9800,89",
+                    "9800,9802,1",
+
+            },
+            delimiter =','
+    )
+        void shouldCalculateSQR (int lowBorder, int topBorder, int expected) {
+        SQRService service = new SQRService();
+        int actual = service.calculateSQR(lowBorder,topBorder);
+        assertEquals (expected, actual);
+    }
+}
